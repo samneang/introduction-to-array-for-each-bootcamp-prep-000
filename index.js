93fc26e09cefc0453e9388741682568859ef28ba
Samneang Seng <seng.samneang12@gmail.com>
2017-03-17 06:45:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function toDoElementArray(array, callback){
+  array.forEach(callback);
+}
+
+function changeCompletely(element, index, array){
+  array[index] = (Math.random() * 100).toString() + '!!!';
+}
+
+var animals = ["dog", "fish", "cat"];
+
+doToElementsInArray(animals, changeCompletely);
+
+console.log(animals);
